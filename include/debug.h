3b5145e09590b3044e3b83d57dc5fdbdf5f1936a
ZetaPC <zgtale1@hotmail.com>
2026-03-15 16:02:55 -0300

    ADD: Debug
    Colores para usar como herramienta de debug

---
+++ include/debug.h
@@ -0,0 +1,8 @@
+//*/* Colores ANSI para C++ */
+constexpr const char* ROJO    = "\033[31m";
+constexpr const char* VERDE   = "\033[32m";
+constexpr const char* AMARILLO= "\033[33m";
+constexpr const char* AZUL    = "\033[34m";
+constexpr const char* MAGENTA = "\033[35m";
+constexpr const char* CIAN    = "\033[36m";
+constexpr const char* NC      = "\033[0m";   // Reset (No Color)
