c99cda5bfdd6f1fcf9c2b50c275ed473c2b02396
VeniSkywalker <venicioroldanslavich@gmail.com>
2026-03-31 15:30:07 -0300

    agenda1 y agenda2

--- src/agendaLS.cpp
+++ src/agendaLS.cpp
@@ -1,32 +1,81 @@
 #include "../include/agendaLS.h"
 
 struct rep_agendaLS {
-    
+    TEvento evento;
+    TAgendaLS sig;
 };
 
 TAgendaLS crearAgendaLS() {
-    return NULL;
+    TAgendaLS nuevo = NULL;
+    return nuevo;
 }
 
 void agregarEnAgendaLS(TAgendaLS &agenda, TEvento evento) {
+    TAgendaLS nuevo = new rep_agendaLS;
+    nuevo->evento=evento;
+    nuevo->sig = NULL;
+    if (agenda == NULL){
+        agenda = nuevo;
+        nuevo = NULL;
+    }else{
+        TAgendaLS p = agenda;
+        if(compararTFechas(fechaTEvento(p->evento), fechaTEvento(evento)) != -1 ){
+            agenda = nuevo;
+            nuevo->sig = p;
+            p=NULL;
+        }else{        
+            while ((p->sig != NULL) && (compararTFechas(fechaTEvento(p->sig->evento), fechaTEvento(evento)) == -1)){
+                p=p->sig;
+            }
+            if (p->sig == NULL){
+                p->sig = nuevo;
+            }
+            else{
+                TAgendaLS q = p->sig;
+                p->sig = nuevo;
+                nuevo->sig = q;
+                p = NULL;
+                q = NULL;
+            }
+        }
+    }
     
 }
 
 
 void imprimirAgendaLS(TAgendaLS agenda) {
-    
+    TAgendaLS p = agenda;
+    while (p!=NULL){
+        imprimirTEvento(p->evento);
+        p=p->sig;
+    }
 }
 
 void liberarAgendaLS(TAgendaLS &agenda) {
-    
+    TAgendaLS p = agenda;
+    TAgendaLS q;
+    while (p!=NULL){
+        liberarTEvento(p->evento);
+        q=p;
+        p=p->sig;
+        delete q;
+        q=NULL;
+    }
 }
 
 bool esVaciaAgendaLS(TAgendaLS agenda){
-    return false;
+    return agenda == NULL;
 }
 
 TAgendaLS copiarAgendaLS(TAgendaLS agenda){
-    return NULL;
+    TAgendaLS copia = crearAgendaLS();
+    TAgendaLS p = agenda;
+    while(p != NULL){
+        agregarEnAgendaLS(copia, copiarTEvento(p->evento));
+        p=p->sig;
+    }
+    p = NULL;
+    return copia;
 }
 
 
